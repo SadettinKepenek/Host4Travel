25d0d0325e874f455f74be94841ee46f96de365f
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-14 20:06:35 +0300

    Add Post CheckIn Validator

---
+++ Host4Travel.BLL/Validators/PostCheckInValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Host4Travel.UI;
+
+namespace Host4Travel.BLL.Validators
+{
+    public class PostCheckInValidator:AbstractValidator<PostCheckIn>
+    {
+        public PostCheckInValidator()
+        {
+            RuleFor(x => x.Application).Cascade(CascadeMode.StopOnFirstFailure).SetValidator(new PostApplicationValidator());
+            RuleFor(x => x.ApplicationId).NotNull().NotEmpty()
+                .WithMessage("CheckIn hangi başvuru için yapıldığı girilmedi.");
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("CheckIn aktiflik durumu belirtilmelidir");
+            RuleFor(x => x.CheckInStartDate).Equal(y => y.Application.ApplicationStartDate)
+                .WithMessage("CheckIn başlama tarihi veya Başvuru başlama tarihi eşit olmalıdır");
+            RuleFor(x => x.CheckInEndDate).Equal(y => y.Application.ApplicationEndDate)
+                .WithMessage("CheckIn bitiş tarihi veya Başvuru bitiş tarihi eşit olmalıdır");
+            
+        }
+    }
+}
