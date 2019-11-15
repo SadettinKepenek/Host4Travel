b8479255ea15bca0146478312a772982c2e2bbd5
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-16 00:51:00 +0300

    Add AddCategoryValidator Add UpdateCategoryValidator Add DeleteCategoryValidator Add Category Mapping Configurations Add CategoryManager Methods

---
+++ Host4Travel.BLL/Validators/CategoryService/DeleteCategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Host4Travel.Core.DTO.CategoryService;
+
+namespace Host4Travel.BLL.Validators.CategoryService
+{
+    public class DeleteCategoryValidator:AbstractValidator<CategoryDeleteDto>
+    {
+        public DeleteCategoryValidator()
+        {
+            RuleFor(x => x.CategoryId).NotNull().NotEmpty().WithMessage("Kategori Identifier Boş geçilemez");
+        }
+    }
+}
--- Host4Travel.Core/MappingProfiles/MappingProfile.cs
+++ Host4Travel.Core/MappingProfiles/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Host4Travel.Core.DTO.CategoryService;
+using Host4Travel.UI;
 
 namespace Host4Travel.Core.MappingProfiles
 {
@@ -6,7 +8,10 @@ namespace Host4Travel.Core.MappingProfiles
     {
         public MappingProfile() {
             // Add as many of these lines as you need to map your objects
-            
+            CreateMap<Category, CategoryListDto>().ReverseMap();
+            CreateMap<CategoryAddDto, Category>();
+            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<CategoryDeleteDto, Category>();
         }
     }
 }
