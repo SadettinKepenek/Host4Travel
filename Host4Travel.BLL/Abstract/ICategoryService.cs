1b7e4d8f3a3dc9a1bd50610ace8b1e458fc9e168
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-16 00:13:09 +0300

    Add ICategoryService Methods

--- Host4Travel.BLL/Abstract/ICategoryService.cs
+++ Host4Travel.BLL/Abstract/ICategoryService.cs
@@ -1,9 +1,16 @@
-﻿using Host4Travel.UI;
+﻿using System.Collections.Generic;
+using Host4Travel.Core.DTO.CategoryService;
+using Host4Travel.UI;
 
 namespace Host4Travel.BLL.Abstract
 {
     public interface ICategoryService
     {
+        List<CategoryListDto> GetAllCategories();
+        CategoryListDto GetCategoryById(int categoryId);
+        void AddCategory(CategoryAddDto model);
+        void UpdateCategory(CategoryUpdateDto model);
+        void DeleteCategory(CategoryDeleteDto model);
         
     }
 }
