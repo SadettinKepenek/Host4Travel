aa4f88a0de1c0948e9b36e8c03fbf6f1c14d9e12
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-18 22:15:16 +0300

    Add PostCategoryRewardManager methods

--- Host4Travel.BLL/Concrete/PostCategoryRewardManager.cs
+++ Host4Travel.BLL/Concrete/PostCategoryRewardManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using AutoMapper;
 using Host4Travel.BLL.Abstract;
+using Host4Travel.BLL.Validators.PostCategoryRewardService;
 using Host4Travel.Core.DTO.PostCategoryRewardService;
 using Host4Travel.Core.ExceptionService.Abstract;
+using Host4Travel.Core.ExceptionService.Exceptions;
 using Host4Travel.DAL.Abstract;
 using Host4Travel.Entities.Concrete;
 using Host4Travel.UI;
@@ -27,24 +29,118 @@ namespace Host4Travel.BLL.Concrete
 
         public List<PostCategoryRewardListDto> GetAllRelations()
         {
-            return null;
+            var entities = _postCategoryRewardDal.GetList();
+            if (entities==null)
+            {
+                return null;
+            }
+
+            var mappedEntities = _mapper.Map<List<PostCategoryRewardListDto>>(entities);
+            return mappedEntities;
         }
 
         public PostCategoryRewardListDto GetRelationById(Guid id)
         {
-            return null;
+            var entity = _postCategoryRewardDal.Get(x => x.PostCategoryRewardId == id);
+            if (entity==null)
+            {
+                return null;
+            }
+
+            var mappedEntity = _mapper.Map<PostCategoryRewardListDto>(entity);
+            return mappedEntity;
         }
 
         public void Add(PostCategoryRewardAddDto dto)
         {
+            try
+            {
+                AddPostCategoryRewardValidator validator=new AddPostCategoryRewardValidator();
+                var validationResult = validator.Validate(dto);
+                if (!validationResult.IsValid)
+                {
+                    throw new ValidationFailureException(validationResult.ToString());
+                }
+                else
+                {
+                    if (_postCategoryRewardDal.IsExists(x=>x.CategoryId==dto.CategoryId && x.RewardId==dto.RewardId && x.PostId==dto.PostId))
+                    {
+                        throw new UniqueConstraintException($"Category Id {dto.CategoryId} ve Reward Id {dto.RewardId} zaten {dto.PostId} için eklenmiş durumda");
+                    }
+                    else
+                    {
+                        var mappedEntity = _mapper.Map<PostCategoryReward>(dto);
+                        _postCategoryRewardDal.Add(mappedEntity);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw _exceptionHandler.HandleServiceException(e);
+            }
         }
 
         public void Update(PostCategoryRewardAddDto dto)
         {
+            if (dto.IsNew)
+            {
+                throw new ArgumentException("IsNew Update İçin false olarak gönderilmelidir");
+            }
+            try
+            {
+                AddPostCategoryRewardValidator validator=new AddPostCategoryRewardValidator();
+                var validationResult = validator.Validate(dto);
+                if (!validationResult.IsValid)
+                {
+                    throw new ValidationFailureException(validationResult.ToString());
+                }
+                else
+                {
+                    if (!_postCategoryRewardDal.IsExists(x=>x.PostCategoryRewardId==dto.PostCategoryRewardId))
+                    {
+                        throw new NullReferenceException($"PostCategoryRewardId {dto.PostCategoryRewardId} için bulunamadı");
+                    }
+                    else
+                    {
+                        var mappedEntity = _mapper.Map<PostCategoryReward>(dto);
+                        _postCategoryRewardDal.Update(mappedEntity);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw _exceptionHandler.HandleServiceException(e);
+            }
         }
 
         public void Delete(PostCategoryRewardDeleteDto dto)
         {
+           
+            try
+            {
+                DeletePostCategoryRewardValidator validator=new DeletePostCategoryRewardValidator();
+                var validationResult = validator.Validate(dto);
+                if (!validationResult.IsValid)
+                {
+                    throw new ValidationFailureException(validationResult.ToString());
+                }
+                else
+                {
+                    if (!_postCategoryRewardDal.IsExists(x=>x.PostCategoryRewardId==dto.PostCategoryRewardId))
+                    {
+                        throw new NullReferenceException($"PostCategoryRewardId {dto.PostCategoryRewardId} için bulunamadı");
+                    }
+                    else
+                    {
+                        var mappedEntity = _mapper.Map<PostCategoryReward>(dto);
+                        _postCategoryRewardDal.Delete(mappedEntity);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw _exceptionHandler.HandleServiceException(e);
+            }
         }
     }
 }
