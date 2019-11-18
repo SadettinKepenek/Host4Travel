dc377323558917addf4fd0d696bb0c50e0350ddc
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-18 23:11:38 +0300

    Add PostsController Methods

--- Host4Travel.API/Controllers/PostsController.cs
+++ Host4Travel.API/Controllers/PostsController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Host4Travel.BLL.Abstract;
+using Host4Travel.Core.DTO.PostService;
 using Host4Travel.Core.ExceptionService.Abstract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -30,7 +32,59 @@ namespace Host4Travel.API.Controllers
             }
             return Ok(posts);
         }
-        // GET
 
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var post = _postService.GetPost(id);
+            if (post==null)
+            {
+                return NotFound($"{id} için kayıt bulunamadı");
+            }
+
+            return Ok(post);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(PostAddDto postAddDto)
+        {
+            try
+            {
+                _postService.AddPost(postAddDto);
+                return Ok("Post Başarıyla Eklendi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(PostUpdateDto postUpdateDto)
+        {
+            try
+            {
+                _postService.UpdatePost(postUpdateDto);
+                return Ok("Post Başarıyla Güncellendi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
+        
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete(PostDeleteDto deleteDto)
+        {
+            try
+            {
+                _postService.DeletePost(deleteDto);
+                return Ok("Post Başarıyla Silindi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
     }
 }
