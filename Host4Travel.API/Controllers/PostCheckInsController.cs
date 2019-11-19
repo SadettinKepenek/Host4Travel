598b965a294c05385c2dcefa2a9a4f916073ecb6
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-19 23:50:56 +0300

    Add PostCheckInsController

---
+++ Host4Travel.API/Controllers/PostCheckInsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Host4Travel.BLL.Abstract;
+using Host4Travel.Core.DTO.PostCheckInService;
+using Host4Travel.Core.ExceptionService.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host4Travel.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostCheckInsController : Controller
+    {
+        private IPostCheckInService _postCheckInService;
+        private IExceptionHandler _exceptionHandler;
+
+        public PostCheckInsController(IPostCheckInService postCheckInService, IExceptionHandler exceptionHandler)
+        {
+            _postCheckInService = postCheckInService;
+            _exceptionHandler = exceptionHandler;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var entities = _postCheckInService.GetAll();
+            if (entities==null)
+            {
+                return NotFound("Herhangi bir sonuç bulunamadı");
+            }
+            return Ok(entities);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] PostCheckInAddDto model)
+        {
+            try
+            {
+                _postCheckInService.Add(model);
+                return Ok("PostCheckIn Başarıyla eklendi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
+        
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody] PostCheckInUpdateDto model)
+        {
+            try
+            {
+                _postCheckInService.Update(model);
+                return Ok("PostCheckIn Başarıyla güncellendi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
+        
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete([FromBody] PostCheckInDeleteDto model)
+        {
+            try
+            {
+                _postCheckInService.Delete(model);
+                return Ok("PostCheckIn Başarıyla silindi");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_exceptionHandler.HandleControllerException(e));
+            }
+        }
+    }
+}
