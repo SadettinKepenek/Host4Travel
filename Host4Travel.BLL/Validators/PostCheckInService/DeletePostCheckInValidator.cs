46e59aefcb79a5e58f08e3b3168c05e4c9b8f676
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-19 23:35:43 +0300

    Add PostCheckIn Validators

---
+++ Host4Travel.BLL/Validators/PostCheckInService/DeletePostCheckInValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Host4Travel.Core.DTO.PostCheckInService;
+
+namespace Host4Travel.BLL.Validators.PostCheckInService
+{
+    public class DeletePostCheckInValidator:AbstractValidator<PostCheckInDeleteDto>
+    {
+        public DeletePostCheckInValidator()
+        {
+            RuleFor(x => x.PostCheckInId).NotNull().NotEmpty().WithMessage("PostCheckInId Boş geçilemez");
+
+        }
+    }
+}
---
+++ Host4Travel.BLL/Validators/PostCheckInService/UpdatePostCheckInValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Host4Travel.Core.DTO.PostCheckInService;
+
+namespace Host4Travel.BLL.Validators.PostCheckInService
+{
+    public class UpdatePostCheckInValidator:AbstractValidator<PostCheckInUpdateDto>
+    {
+        public UpdatePostCheckInValidator()
+        {
+            RuleFor(x => x.PostCheckInId).NotNull().NotEmpty().WithMessage("PostCheckInId Boş geçilemez");
+            RuleFor(x => x.ApplicationId).NotNull().NotEmpty()
+                .WithMessage("CheckIn hangi başvuru için yapıldığı girilmedi.");
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("CheckIn aktiflik durumu belirtilmelidir");
+            RuleFor(x => x.CheckInStartDate).Equal(y => y.CheckInStartDate)
+                .WithMessage("CheckIn başlama tarihi veya Başvuru başlama tarihi eşit olmalıdır");
+            RuleFor(x => x.CheckInEndDate).Equal(y => y.CheckInEndDate)
+                .WithMessage("CheckIn bitiş tarihi veya Başvuru bitiş tarihi eşit olmalıdır");
+        }
+    }
+}
