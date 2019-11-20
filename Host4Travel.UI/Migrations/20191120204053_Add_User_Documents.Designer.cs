e9750e24781b95bdebd877c01275793194575247
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-20 23:42:36 +0300

    Add Relations

--- Host4Travel.UI/Migrations/20191120204053_Add_User_Documents.Designer.cs
+++ Host4Travel.UI/Migrations/20191120204053_Add_User_Documents.Designer.cs
@@ -4,14 +4,16 @@ using Host4Travel.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Host4Travel.UI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191120204053_Add_User_Documents")]
+    partial class Add_User_Documents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
