1cb3662cee650a61b87afb2783fceacee3afd34c
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-21 00:02:21 +0300

    Add Document Type Validators

---
+++ Host4Travel.BLL/Validators/DocumentService/DeleteDocumentValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Host4Travel.BLL.Validators.DocumentService
+{
+    public class DeleteDocumentValidator
+    {
+        
+    }
+}
---
+++ Host4Travel.BLL/Validators/DocumentTypeService/UpdateDocumentTypeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Host4Travel.Core.DTO.DocumentService;
+using Host4Travel.Core.DTO.DocumentTypeService;
+
+namespace Host4Travel.BLL.Validators.DocumentTypeService
+{
+    public class UpdateDocumentTypeValidator:AbstractValidator<DocumentTypeUpdateDto>
+    {
+        public UpdateDocumentTypeValidator()
+        {
+            RuleFor(x => x.DocumentTypeId).NotNull().NotEmpty();
+            RuleFor(x => x.DocumentTypeName).NotNull().NotEmpty().WithMessage("Document Type Name boş geçilemez");
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("Document Type Aktiflik durumu boş geçilemez");
+        }
+    }
+}
