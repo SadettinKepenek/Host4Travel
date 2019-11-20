dfd1763bd21ca36575907b45a54e4dff022725ef
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-20 23:30:29 +0300

    Add Document Entity Add DocumentType Entity

---
+++ Host4Travel.Entities/Concrete/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Host4Travel.Entities.Concrete
+{
+    public class Document
+    {
+        [Key]
+        public Guid DocumentId { get; set; }
+        public string DocumentUrl { get; set; }
+        public DateTime DocumentUploadDate { get; set; }
+        public bool IsVerified { get; set; }
+        public bool IsActive { get; set; }
+        public string OwnerId { get; set; }
+        public int DocumentTypeId { get; set; }
+        
+        public virtual DocumentType DocumentType { get; set; }
+        public virtual ApplicationIdentityUser Owner { get; set; }
+    }
+}
