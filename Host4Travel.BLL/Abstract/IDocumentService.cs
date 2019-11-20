4a29734b1d18dfa1bfc1e70f3a212411d2b31964
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-21 00:07:32 +0300

    Add IDocument Service Add IDocument Type Service

---
+++ Host4Travel.BLL/Abstract/IDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Host4Travel.Core.DTO.DocumentService;
+
+namespace Host4Travel.BLL.Abstract
+{
+    public interface IDocumentService
+    {
+        List<DocumentListDto> GetAll();
+        DocumentListDto GetById();
+        void Add(DocumentAddDto model);
+        void Update(DocumentUpdateDto model);
+        void Delete(DocumentUpdateDto model);
+    }
+}
---
+++ Host4Travel.Core/DTO/DocumentTypeService/DocumentTypeListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Host4Travel.Core.DTO.DocumentTypeService
+{
+    public class DocumentTypeListDto
+    {
+        public int DocumentTypeId { get; set; }
+        public string DocumentTypeName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
