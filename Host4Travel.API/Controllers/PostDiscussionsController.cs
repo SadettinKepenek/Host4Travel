ea403abb3843e9f06aeb3ad94af1068fcd45ff04
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-11-23 15:13:30 +0300

    Add Response Model to Post Discussions

--- Host4Travel.API/Controllers/PostDiscussionsController.cs
+++ Host4Travel.API/Controllers/PostDiscussionsController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using Host4Travel.API.Models.ResponseModels;
 using Host4Travel.BLL.Abstract;
 using Host4Travel.Core.DTO.PostDiscussionService;
 using Host4Travel.Core.ExceptionService.Abstract;
@@ -28,10 +31,17 @@ namespace Host4Travel.API.Controllers
             var entities = _postDiscussionService.GetAll();
             if (entities == null)
             {
-                return NotFound("Herhangi bir kayıt bulunamadı");
+                return NotFound(new ResponseModel
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Message = "Kayıt bulunamadı"
+                });
             }
-
-            return Ok(entities);
+            ResponseModelWithData<List<PostDiscussionListDto>> responseModelWithData = new ResponseModelWithData<List<PostDiscussionListDto>>();
+            responseModelWithData.StatusCode = HttpStatusCode.OK;
+            responseModelWithData.Message = "Kayıtlar başarıla getirildi";
+            responseModelWithData.Data = entities;
+            return Ok(responseModelWithData);
         }
 
         [HttpPost("Add")]
@@ -39,12 +49,19 @@ namespace Host4Travel.API.Controllers
         {
             try
             {
+                ResponseModel responseModel = new ResponseModel();
                 _postDiscussionService.Add(model);
-                return Ok("Post Discussion başarıyla eklendi");
+                responseModel.StatusCode = HttpStatusCode.OK;
+                responseModel.Message = "Post Discussion başarıyla eklendi";
+                return Ok(responseModel);
             }
             catch (Exception e)
             {
-                return BadRequest(_exceptionHandler.HandleControllerException(e));
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = _exceptionHandler.HandleControllerException(e)
+                });
             }
         }
 
@@ -53,12 +70,19 @@ namespace Host4Travel.API.Controllers
         {
             try
             {
+                ResponseModel responseModel = new ResponseModel();
                 _postDiscussionService.Update(dto);
-                return Ok("Post Discussion başarıyla güncellendi");
+                responseModel.StatusCode = HttpStatusCode.OK;
+                responseModel.Message = "Post Discussion başarıyla güncellendi";
+                return Ok(responseModel);
             }
             catch (Exception e)
             {
-                return BadRequest(_exceptionHandler.HandleControllerException(e));
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = _exceptionHandler.HandleControllerException(e)
+                });
             }
         }
         [HttpDelete("Delete")]
@@ -66,12 +90,19 @@ namespace Host4Travel.API.Controllers
         {
             try
             {
+                ResponseModel responseModel = new ResponseModel();
                 _postDiscussionService.Delete(dto);
-                return Ok("Post Discussion Başarıyla silindi");
+                responseModel.StatusCode = HttpStatusCode.OK;
+                responseModel.Message = "Post Discussion başarıyla silindi";
+                return Ok(responseModel);
             }
             catch (Exception e)
             {
-                return BadRequest(_exceptionHandler.HandleControllerException(e));
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = _exceptionHandler.HandleControllerException(e)
+                });
             }
         }
     }
