a08a819604fe1df0759eaa62f258a5865ad2a847
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-25 23:40:18 +0300

    Modify ApplicationIdentityUserListDto

--- Host4Travel.Core/DTO/AuthService/ApplicationIdentityUserListDto.cs
+++ Host4Travel.Core/DTO/AuthService/ApplicationIdentityUserListDto.cs
@@ -1,4 +1,13 @@
-﻿namespace Host4Travel.Core.DTO.AuthService
+﻿using System;
+using System.Collections.Generic;
+using Host4Travel.Core.DTO.DocumentService;
+using Host4Travel.Core.DTO.PostApplicationService;
+using Host4Travel.Core.DTO.PostRatingService;
+using Host4Travel.Core.DTO.PostService;
+using Host4Travel.Entities.Concrete;
+using Host4Travel.UI;
+
+namespace Host4Travel.Core.DTO.AuthService
 {
     public class ApplicationIdentityUserListDto
     {
@@ -6,5 +15,14 @@
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
+        public string CookieAcceptIpAddress { get; set; }
+        public DateTime CookieAcceptDate { get; set; }
+        public bool IsCookieAccepted { get; set; }
+        public bool IsVerified { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<PostListDto> Post { get; set; }
+        public virtual ICollection<PostApplicationListDto> PostApplication { get; set; }
+        public virtual ICollection<PostRatingListDto> PostRating { get; set; }
+        public virtual ICollection<DocumentListDto> Documents { get; set; }
     }
 }
