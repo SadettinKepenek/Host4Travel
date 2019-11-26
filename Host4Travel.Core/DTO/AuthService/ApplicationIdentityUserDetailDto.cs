75e1767778e61c9f2f5fcc66aaa9a02b394e3cfb
berkayalcin <yalcinberkaycom@gmail.com>
2019-11-26 23:09:07 +0300

    Done

--- Host4Travel.Core/DTO/AuthService/ApplicationIdentityUserDetailDto.cs
+++ Host4Travel.Core/DTO/AuthService/ApplicationIdentityUserDetailDto.cs
@@ -12,7 +12,5 @@ namespace Host4Travel.Core.DTO.AuthService
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
-        public virtual ICollection<PostListDto> Post { get; set; }
-        public virtual ICollection<PostRatingListDto> PostRating { get; set; }
     }
 }
