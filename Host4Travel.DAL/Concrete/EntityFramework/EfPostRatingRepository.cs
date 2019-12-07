e0e10795c04c9b8f567c19700b651e800f7e55e1
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-07 18:08:16 +0300

    Modify EfPostRatingRepository

--- Host4Travel.DAL/Concrete/EntityFramework/EfPostRatingRepository.cs
+++ Host4Travel.DAL/Concrete/EntityFramework/EfPostRatingRepository.cs
@@ -1,11 +1,33 @@
-﻿using Host4Travel.Core.DAL.Concrete.EntityFramework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Host4Travel.Core.DAL.Concrete.EntityFramework;
 using Host4Travel.DAL.Abstract;
 using Host4Travel.UI;
+using Microsoft.EntityFrameworkCore;
 
 namespace Host4Travel.DAL.Concrete.EntityFramework
 {
     public class EfPostRatingRepository:EfEntityRepositoryBase<PostRating,ApplicationDbContext>,IPostRatingDal
     {
-        
+        public List<PostRating> GetList(Expression<Func<PostRating, bool>> filter = null)
+        {
+            using ApplicationDbContext context = new ApplicationDbContext();
+            if (filter == null)
+            {
+                return context.PostRating
+                    .Include(x => x.Application)
+                    .Include(x => x.Owner)
+                    .Include(x => x.Post)
+                    .ToList();
+            }
+            return context.PostRating
+                .Include(x => x.Application)
+                .Include(x => x.Owner)
+                .Include(x => x.Post)
+                .Where(filter)
+                .ToList();
+        }
     }
 }
