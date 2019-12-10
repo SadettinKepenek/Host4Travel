3c162ab05b164a4bb7dc14ccff5ee12c3ac85700
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-11 01:04:57 +0300

    Add Add Document Action to Document Controller

---
+++ Host4Travel.API/Controllers/DocumentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Host4Travel.API.Models.ResponseModels;
+using Host4Travel.BLL.Abstract;
+using Host4Travel.Core.DTO.DocumentDtos;
+using Host4Travel.Core.ExceptionService.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host4Travel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private IDocumentService _documentService;
+        private IExceptionHandler _exceptionHandler;
+
+        public DocumentsController(IDocumentService documentService, IExceptionHandler exceptionHandler)
+        {
+            _documentService = documentService;
+            _exceptionHandler = exceptionHandler;
+        }
+
+        [HttpPost("Add")]
+        [Authorize]
+        public async Task<IActionResult> AddDocument([FromBody] DocumentAddDto model)
+        {
+            try
+            {
+                ResponseModel responseModel = new ResponseModel();
+                _documentService.Add(model);
+                responseModel.StatusCode = HttpStatusCode.OK;
+                responseModel.Message = "Document Başarıyla Eklendi";
+                return Ok(responseModel);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = _exceptionHandler.HandleControllerException(e)
+                });
+            }
+        }
+    }
+}
